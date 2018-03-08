3004e65132ea048da10d8725d3906dd397f89a0e
Brennan Davis <brennan.davis1410@gmail.com>
2018-03-08 14:08:04 -0700

    small tweaks to command prompts; updated version number

--- pymount.py
+++ pymount.py
@@ -41,7 +41,7 @@ def generate_config(config_file):
     ssh_host = raw_input("Please enter an ssh host (e.g. user@server:/path/to/remote/dir): ")
     local_mount_dir = raw_input("Please enter a local directory path to mount remote directories to (e.g. /Users/username/Development): ")
     if not os.path.exists(local_mount_dir):
-        create_local_mount_dir = raw_input("The local directory you entered does not exist. Create directory? (y/n)")
+        create_local_mount_dir = raw_input("The local directory you entered does not exist. Create directory? (y/n) ")
         if create_local_mount_dir == 'y':
             os.system("mkdir %s" % local_mount_dir)
     
